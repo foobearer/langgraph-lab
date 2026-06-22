feb58622d3298d1b36ccd40a0172cd48e08ad8f9
foobearer <paragasjoycee1@gmail.com>
2026-06-22 17:41:25 +0400

    created a single agent that uses wikipedia as a tool

---
+++ single-agent.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+import wikipedia
+from langchain_core.tools import tool
+
+wikipedia.set_user_agent("langgraph-lab/1.0 (https://github.com/)")
+
+@tool 
+def wikipedia_tool(
+    query: Annotated[str, "The wikipedia search to execute to find key summary information"],
+):
+    """Use this to search Wikipedia for actual information"""
+
+    try: 
+
+        results = wikipedia.search(query)
+
+        if not results:
+            return "No results found on Wikipedia."
+        
+        title = results[0]
+
+        summary = wikipedia.summary(title, sentences=8, auto_suggest=False, redirect=True)
+
+
+    except BaseException as e:
+        return f"Failed to execute. Error: {repr(e)}"
+    return f"Successfully executed:\nWikipedia summary: {summary}"
+
+
+company_name = "Warner bros."
+wiki_summary = wikipedia_tool.invoke(f"{company_name}")
+print(wiki_summary)
